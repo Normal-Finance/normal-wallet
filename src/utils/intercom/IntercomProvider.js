8066c651afa51fc73f7f4243b66ac646eafc0899
Joshua Blew <joshua@normalfinance.io>
2023-06-15 14:31:30 -0500

    comment out erroring intercom code

--- src/utils/intercom/IntercomProvider.js
+++ src/utils/intercom/IntercomProvider.js
@@ -5,28 +5,28 @@ import { useRouter } from 'next/router';
 import { load as loadIntercom, boot as bootIntercom, update as updateIntercom } from './intercom';
 
 export const IntercomProvider = ({ children }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
   if (typeof window !== 'undefined') {
     loadIntercom();
     bootIntercom();
   }
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (typeof window !== 'undefined') {
-        updateIntercom();
-      }
-    };
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     if (typeof window !== 'undefined') {
+  //       updateIntercom();
+  //     }
+  //   };
 
-    router.onRouteChangeStart(handleRouteChange);
+  //   router.onRouteChangeStart(handleRouteChange);
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.onRouteChangeStart(handleRouteChange);
-    };
-  }, [router.events]);
+  //   // If the component is unmounted, unsubscribe
+  //   // from the event with the `off` method:
+  //   return () => {
+  //     router.onRouteChangeStart(handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return children;
 };
