36768fddf017aaa80fa3999b343af44826b70def
Joshua Blew <joshua@normalfinance.io>
2023-06-22 16:17:21 -0500

    cleanups

--- src/contexts/WalletContext.tsx
+++ src/contexts/WalletContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 import { WalletInstance, useWallet, useConnectionStatus } from '@thirdweb-dev/react';
 import { SmartWallet } from '@thirdweb-dev/wallets';
--- src/contexts/WebsocketContext.tsx
+++ src/contexts/WebsocketContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 // redux
