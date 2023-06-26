e1ac8bc0501afa5cd1f4d0faf83301f55c4ef7b0
Joshua Blew <joshua@normalfinance.io>
2023-06-26 12:24:38 -0500

    remove unused type declerations

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -1,6 +1,3 @@
-import { IAddressItem } from './address';
-
-// ----------------------------------------------------------------------
 
 export type IWalletTableFilterValue = string | string[] | Date | null;
 
@@ -8,33 +5,4 @@ export type IWalletTableFilters = {
   name: string;
   service: string[];
   status: string;
-};
-
-// ----------------------------------------------------------------------
-
-export type IWalletItem = {
-  id: string;
-  title: string;
-  price: number;
-  total: number;
-  service: string;
-  quantity: number;
-  description: string;
-};
-
-export type IWallet = {
-  id: string;
-  sent: number;
-  status: string;
-  totalAmount: number;
-  invoiceNumber: string;
-  subTotal: number;
-  items: IWalletItem[];
-  taxes: number | string;
-  dueDate: Date | number;
-  discount: number | string;
-  shipping: number | string;
-  createDate: Date | number;
-  invoicetTo: IAddressItem;
-  invoiceFrom: IAddressItem;
 };
